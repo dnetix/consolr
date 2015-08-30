d37d79104e004fb74aeedd86d06a31bbc6469bea
Diego Calle <dnetix@gmail.com>
2015-08-29 22:33:50 -0500

    Added primitive functionality to accept JSON

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 var events = require('events');
+var fJSON = require('fbbk-json');
 
 var consolr = function(){
 	var self = this;
+	this.debug = false;
 	process.stdin.on('data', function(chunk){
-		var pattern = /(?:\:)(\w+)(?:\s)?([\w \.\,\"]+)?/gi;
+		var pattern = /(?:\:)(\w+)(?:\s)?([\w \.\,\"\{\}\:\'\[\]]+)?/gi;
 		var text = chunk.toString().replace(/(\r\n|\n|\r)/g, "");
+		var eventName = 'command';
 		if(matched = pattern.exec(text)){
-			self.emit(matched[1], (matched[2] == undefined ? null : matched[2]));
-		}else{
-			self.emit('command', text);
+			eventName = matched[1];
+			text = matched[2];
 		}
+		
+		try {
+			var json = fJSON.parse(text);
+			text = json;
+		}catch(ex){
+			if(self.debug){
+				console.log(ex);
+			}
+		}
+
+		self.emit(eventName, text);
 	});
 };
 
 consolr.prototype = new events.EventEmitter;
 
 module.exports = new consolr();
+
